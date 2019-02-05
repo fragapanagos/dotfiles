9a8ffcd8569b2c0b373eeeb81f1b716ac5ef4c56
Sam Fok <sam.b.fok@gmail.com>
2019-02-05 07:07:52 +0000

    add vimrc tmux.conf to setup

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-symlink vim/ftplugin and vim/syntax to ~/.vim/ftplugin and ~/.vim/syntax
-
-For git-completion.bash to work, download 
-https://raw.github.com/git/git/master/contrib/completion/git-completion.bash to ~/.git-completion.bash
+run `./setup.sh` for installation
--- setup.sh
+++ setup.sh
@@ -1,10 +1,15 @@
 #/bin/bash
 
-# wget https://raw.github.com/git/git/master/contrib/completion/git-completion.bash -O ~/.git-completion.bash
+wget https://raw.github.com/git/git/master/contrib/completion/git-completion.bash -O ~/.git-completion.bash
 
-# cat bashrc_git >> ~/.bashrc
+cat bashrc_git >> ~/.bashrc
 
 git config --global push.default simple
 git config --global core.editor "vim"
 git config user.email sam.b.fok@gmail.com
 git config user.name "Sam Fok"
+
+ln -s $PWD/vimrc ~/.vimrc
+ln -s $PWD/vim ~/.vim
+
+ln -s $PWD/tmux.conf ~/.tmux.conf
