f0175c07e495a85d3a3a85eb1bdc798df4dac73f
Sam Fok <sam.b.fok@gmail.com>
2019-02-05 06:58:18 +0000

    git config setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #/bin/bash
 
-wget https://raw.github.com/git/git/master/contrib/completion/git-completion.bash -O ~/.git-completion.bash
+# wget https://raw.github.com/git/git/master/contrib/completion/git-completion.bash -O ~/.git-completion.bash
 
-cat bashrc_git >> ~/.bashrc
+# cat bashrc_git >> ~/.bashrc
+
+git config --global push.default simple
+git config --global core.editor "vim"
+git config user.email sam.b.fok@gmail.com
+git config user.name "Sam Fok"
