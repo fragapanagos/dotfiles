ba0f831a689fa340392ade6489acf02675568483
Sam Fok <sam.b.fok@gmail.com>
2020-08-30 19:18:08 -0700

    tmux open in current dir

--- setup.sh
+++ setup.sh
@@ -1,14 +1,5 @@
 #/bin/bash
 
-wget https://raw.github.com/git/git/master/contrib/completion/git-completion.bash -O ~/.git-completion.bash
-
-cat bashrc_git >> ~/.bashrc
-
-git config --global push.default simple
-git config --global core.editor "vim"
-git config user.email sam.b.fok@gmail.com
-git config user.name "Sam Fok"
-
 ln -s $PWD/vimrc ~/.vimrc
 ln -s $PWD/vim ~/.vim
 
